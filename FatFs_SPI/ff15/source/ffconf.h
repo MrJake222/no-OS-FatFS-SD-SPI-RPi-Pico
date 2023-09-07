f3ac70784944e8663a30dde8004ef4dd6a705a3d
MrJake222 <norbertzpilicy@gmail.com>
2023-09-07 15:03:42 +0200

    turned on freertos

--- FatFs_SPI/ff15/source/ffconf.h
+++ FatFs_SPI/ff15/source/ffconf.h
@@ -275,8 +275,8 @@
 /      lock control is independent of re-entrancy. */
 
 
-#define FF_FS_REENTRANT	0
-#define FF_FS_TIMEOUT	1000
+#define FF_FS_REENTRANT	1
+#define FF_FS_TIMEOUT	(1000 / portTICK_PERIOD_MS)
 /* The option FF_FS_REENTRANT switches the re-entrancy (thread safe) of the FatFs
 /  module itself. Note that regardless of this option, file access to different
 /  volume is always re-entrant and volume control functions, f_mount(), f_mkfs()
